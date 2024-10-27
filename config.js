27e3f06e56203bf8165979f43933f5f98c0e4e1a
Starboy2010 <166742048+Starboy2010@users.noreply.github.com>
2024-10-27 20:10:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ require("./database/module")
 
 //GLOBAL PAYMENT
 global.storename = "STINGER-V4"
-global.dana = "2349037524605"
+global.dana = "254105509658"
 global.qris = "https://i.ibb.co/BnYRXcw/STINGER-V4.jpg"
 
 
 // GLOBAL SETTING
-global.owner = "2349037524605"
+global.owner = "254105509658"
 global.namabot = " S҉ T҉ I҉ N҉ G҉ E҉ R҉- V҉ 4҉"
-global.nomorbot = "2349037524605"
+global.nomorbot = "254105509658"
 global.namaCreator = "ES TEAMS"
 global.linkyt = "https://youtube.com/@esteams"
 global.autoJoin = false
@@ -32,7 +32,7 @@ global.location = '1'
 
 global.codeInvite = ""
 global.imageurl = 'https://i.ibb.co/BnYRXcw/STINGER-V4.jpg'
-global.isLink = 'https://whatsapp.com/channel/0029Vaj1vKSK5cDDT4tVvY1y'
+global.isLink = 'https://whatsapp.com/channel/0029VafjXkNKgsNotjWfOW0R'
 global.packname = "ES TEAMS"
 global.author = "ES TEAMS"
 global.jumlah = "5"
